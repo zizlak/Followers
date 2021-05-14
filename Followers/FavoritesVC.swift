9e46f7a3f458cf94234040296bfb4845dc1086fb
Aleksandr Kurdiukov <runoleonard@gmail.com>
2021-05-14 15:23:49 +0200

    2tabs

---
+++ Followers/FavoritesVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesVC.swift
+//  Followers
+//
+//  Created by Aleksandr Kurdiukov on 14.05.21.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+}
--- Followers/SceneDelegate.swift
+++ Followers/SceneDelegate.swift
@@ -16,8 +16,37 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        
+        let tabVC = UITabBarController()
+        
+        tabVC.viewControllers = [createSearchNVCs(), createFavoritesNVCs()]
+        
+        
+        window?.rootViewController = tabVC
+        window?.makeKeyAndVisible()
     }
+    
+    
+    private func createSearchNVCs() -> UINavigationController {
+        let vc = SearchVC()
+        vc.title = "Search"
+        vc.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: vc)
+    }
+    
+    private func createFavoritesNVCs() -> UINavigationController {
+        let vc = FavoritesVC()
+        vc.title = "Favorites"
+        vc.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: vc)
+    }
+    
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
